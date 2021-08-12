940bf2fa260d19aec4a489cd53c61591b0993d8a
codedsakura <sakuracoded@gmail.com>
2021-08-12 10:54:10 +0300

    add backwards compatibility to warps

--- src/main/java/eu/codedsakura/codedsmputils/modules/teleportation/warps/WarpListComponent.java
+++ src/main/java/eu/codedsakura/codedsmputils/modules/teleportation/warps/WarpListComponent.java
@@ -1,33 +1,39 @@
 package eu.codedsakura.codedsmputils.modules.teleportation.warps;
 
 import net.fabricmc.fabric.api.util.NbtType;
+import net.minecraft.SharedConstants;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.PersistentState;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WarpListComponent extends PersistentState {
     private static final String FILE_KEY = "csmpu_warps";
-//    private static final Set<ServerWorld> backwardsWorlds = new HashSet<>();
+    private static final Set<ServerWorld> backwardsWorlds = new HashSet<>();
 
     public static WarpListComponent get(ServerWorld world) {
-//        WarpListComponent backwards = null;
-//        if (!backwardsWorlds.contains(world)) {
-//            backwardsWorlds.add(world);
-//            try {
-//                NbtCompound nbt = world.getPersistentStateManager().readNbt("cardinal_world_components",
-//                        SharedConstants.getGameVersion().getWorldVersion());
-//                int ignored = 0;
-//            } catch (IOException ignored) {}
-//            backwards = world.getPersistentStateManager().get(WarpListComponent::readBackwardsNbt, "cardinal_world_components");
-//            if (backwards == null) backwards = world.getPersistentStateManager().get(WarpListComponent::readNbt, "cardinal_world_components");
-//        }
-//        WarpListComponent finalBackwards = backwards;
-        return world.getPersistentStateManager().getOrCreate(WarpListComponent::readNbt, WarpListComponent::new, FILE_KEY);
+        WarpListComponent backwards = null;
+        if (!backwardsWorlds.contains(world)) {
+            backwardsWorlds.add(world);
+            try {
+                NbtCompound nbt = world.getPersistentStateManager().readNbt("cardinal_world_components",
+                        SharedConstants.getGameVersion().getWorldVersion());
+                if (nbt != null) {
+                    backwards = WarpListComponent.readBackwardsNbt(nbt.getCompound("data"));
+                }
+            } catch (IOException ignored) {
+            }
+        }
+        WarpListComponent finalBackwards = backwards == null ? new WarpListComponent() :
+                (backwards.warps.size() > 0 ? backwards : new WarpListComponent());
+        return world.getPersistentStateManager().getOrCreate(WarpListComponent::readNbt, () -> finalBackwards, FILE_KEY);
     }
 
     public static WarpListComponent readNbt(NbtCompound nbt) {
@@ -47,19 +53,19 @@ public class WarpListComponent extends PersistentState {
         return true;
     }
 
-//    public static WarpListComponent readBackwardsNbt(NbtCompound nbt) {
-//        WarpListComponent warp = new WarpListComponent();
-//        NbtCompound tag = nbt.getCompound("cardinal_components");
-//        if (tag.contains("fabricwarps:warplist")) {
-//            warp.readFromNbt(nbt.getCompound("fabricwarps:warplist"));
-//            return warp;
-//        }
-//        if (tag.contains("fabric-smp-utils:warps")) {
-//            warp.readFromNbt(nbt.getCompound("fabric-smp-utils:warps"));
-//            return warp;
-//        }
-//        return null;
-//    }
+    public static WarpListComponent readBackwardsNbt(NbtCompound nbt) {
+        WarpListComponent warp = new WarpListComponent();
+        NbtCompound tag = nbt.getCompound("cardinal_components");
+        if (tag.contains("fabricwarps:warplist")) {
+            warp.readFromNbt(tag.getCompound("fabricwarps:warplist"));
+            return warp;
+        }
+        if (tag.contains("fabric-smp-utils:warps")) {
+            warp.readFromNbt(tag.getCompound("fabric-smp-utils:warps"));
+            return warp;
+        }
+        return null;
+    }
 
     private final List<Warp> warps = new ArrayList<>();
 
